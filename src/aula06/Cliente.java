594c75fb96eaca8fa7f06986604136e56f8f7a21
ulisses guimaraes <ulisses.guimaraes@rede.ulbra.br>
2026-04-08 01:03:11 -0300

    meu primeiro commit

--- src/aula06/Cliente.java
+++ src/aula06/Cliente.java
@@ -1,17 +1,38 @@
-package aula06;
+package aula6;
 
 public class Cliente {
     public String nome;
     public String cpf;
-    //public Float saldo;
+    public Float saldo;
 
-
-    public Cliente(String nome, String cpf){
-        this.nome = nome;
-        this.cpf = cpf;
-
-
-
+    public Cliente() {
     }
 
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    public Cliente(String cpf, String nome) {
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+    public Cliente(Float saldo) {
+        this.saldo = saldo;
+    }
+
+    public Cliente(String nome, String cpf, Float saldo) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", saldo=" + saldo +
+                '}';
+    }
 }
--- src/aula06/aula06Main.java
+++ src/aula06/aula06Main.java
@@ -1,48 +1,106 @@
-import aula06.Cliente;
-
-import javax.lang.model.element.NestingKind;
+import aula6.Cliente;
 
 void main() {
     ArrayList<Cliente> clientes = new ArrayList<>();
 
     boolean desejaSair = false;
 
-    while (!desejaSair){
+    while (!desejaSair) {
+        int opcaoEscolhdida = menu();
 
-        int opcaoEscolhida = menu();
-
-        switch (opcaoEscolhida){
-
+        switch (opcaoEscolhdida) {
             case 1:
-                //
-                String nome = IO.readln("Digite o nome do novo cliente: \n");
-                String cpf = IO.readln("Digite o cpf do cliente: \n");
-
-                clientes.add(new Cliente(nome,cpf));
+                salvarCliente(clientes);
                 break;
             case 2:
-                //
+                listarClientes(clientes);
                 break;
-            case 0:
+            case 3:
+                removerCliente(clientes);
+                break;
+
+            case 4:
+                editarCliente(clientes);
+                break;
+            case 5:
                 desejaSair = true;
         }
 
-
-
     }
+}
 
+public void salvarCliente(ArrayList<Cliente> clientes) {
+    String nome = IO.readln("Digite o nome do cliente: ");
+    String cpf = IO.readln("Digite o cpf do cliente: ");
+    Float saldo = 0f;
+
+    Cliente cliente = new Cliente(nome, cpf, saldo);
+
+    clientes.add(cliente);
+    System.out.println("Cliente salvo com sucesso");
 
 }
-public int menu(){
-    IO.println("Digite umas das opções abaixo:");
-    IO.println("1 = Adicionar novo cliente");
-    IO.println("2 = Listar clientes");
-    IO.println("3 = Remover cliente");
-    IO.println("4 = Editar cliente");
-    IO.println("0 = Sair");
+
+public void listarClientes(ArrayList<Cliente> clientes) {
+    for (int i = 0; i < clientes.size(); i++) {
+        System.out.println(clientes.get(i));
+        IO.println(clientes.get(i));
+    }
+}
+
+public void removerCliente(ArrayList<Cliente> clientes) {
+    String cpf = IO.readln("Informe o cpf do cliente: ");
+    boolean encontrouCliente = false;
+    for (int i = 0; i < clientes.size(); i++) {
+        if (clientes.get(i).cpf.equals(cpf)) {
+            clientes.remove(i);
+            encontrouCliente = true;
+        }
+    }
+
+    if(!encontrouCliente) {
+        System.out.println("Cliente nao encontrado");
+    }else {
+        System.out.println("Cliente removido com sucesso!");
+    }
+}
+public void editarCliente(ArrayList<Cliente> clientes) {
+    String cpf = IO.readln("Digite o CPF do cliente que deseja editar: ");
+    boolean encontrouCliente = false;
+
+    for (int i = 0; i < clientes.size(); i++) {
+        Cliente cliente = clientes.get(i);
+
+        if (cliente.cpf.equals(cpf)) {
+            encontrouCliente = true;
+
+            System.out.println("Cliente encontrado: " + cliente);
+
+            String novoNome = IO.readln("Digite o novo nome: ");
+            String novoCpf = IO.readln("Digite o novo CPF: ");
+            Float novoSaldo = Float.parseFloat(IO.readln("Digite o novo saldo: "));
+
+            cliente.nome = novoNome;
+            cliente.cpf = novoCpf;
+            cliente.saldo = novoSaldo;
+
+            System.out.println("Cliente atualizado com sucesso!");
+            break;
+        }
+    }
+
+    if (!encontrouCliente) {
+        System.out.println("Cliente não encontrado!");
+    }
+}
+public int menu() {
+    System.out.println("Digite uma das opcoes abaixo: ");
+    System.out.println("1 = Adicionar novo cliente");
+    System.out.println("2 = Listar clientes");
+    System.out.println("3 = Remover cliente");
+    System.out.println("4 = Editar cliente");
+    System.out.println("5 = Sair");
 
     int opcaoEscolhida = Integer.parseInt(IO.readln());
     return opcaoEscolhida;
-
-
 }
